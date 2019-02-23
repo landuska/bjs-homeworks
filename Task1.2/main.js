910ceddc479b581bf091f65afaed84cc675e0bfc
Landysh Khusaenova <landysh.kg@gmail.com>
2019-02-24 00:14:02 +0300

    Second task - done and commit

---
+++ Task1.2/main.js
@@ -0,0 +1,14 @@
+let algebra;
+let geography;
+let physics;
+let averageMark;
+let countOfSubjects; //количество предметов в школе//
+
+algebra = 5;
+geography = 4;
+physics = 5;
+countOfSubject = 3;
+
+//средняя оценка за три предмета//
+averageMark = (algebra + geography + physics) / countOfSubject;
+console.log(averageMark);
